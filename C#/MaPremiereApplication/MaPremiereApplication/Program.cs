154557e26d15cfb548e45e470c03a99f7b2cdb94
William Bruel <bruel.william@gmail.com>
2018-11-12 21:14:52 +0100

    Creation Resp Learning

---
+++ C#/MaPremiereApplication/MaPremiereApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaPremiereApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> chiffres = new List<int>(); // création de la liste
+            chiffres.Add(8); // chiffres contient 8
+            chiffres.Add(9); // chiffres contient 8, 9
+            chiffres.Add(4); // chiffres contient 8, 9, 4
+
+            chiffres.RemoveAt(1); // chiffres contient 8, 4
+
+            foreach (int chiffre in chiffres)
+            {
+                Console.WriteLine(chiffre);
+            }
+        }
+    }
+}
