a7a05b63f763bac4cc325cbcfdecef75f4a69ff3
WilliamB1994 <39240575+WilliamB1994@users.noreply.github.com>
2018-11-12 21:48:25 +0100

    mdr

--- C#/MaPremiereApplication/MaPremiereApplication/Program.cs
+++ C#/MaPremiereApplication/MaPremiereApplication/Program.cs
@@ -13,7 +13,7 @@ namespace MaPremiereApplication
             List<int> chiffres = new List<int>(); // création de la liste
             chiffres.Add(8); // chiffres contient 8
             chiffres.Add(9); // chiffres contient 8, 9
-            chiffres.Add(4); // chiffres contient 8, 9, 4
+            chiffres.Add(4); // chiffres contient 8, 9, 4, 5
 
             chiffres.RemoveAt(1); // chiffres contient 8, 4
 
